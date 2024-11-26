f2655b382a9f69ce26afab7486aaf7bb2dfe2968
i-Ac1D-i <acidondiscord99@gmail.com>
2024-11-26 20:49:38 +0100

    Added Burger Menu for Navbar on lower width

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  document.getElementById("burger-menu").addEventListener("click", function () {
+    const links = document.getElementById("navbar-links");
+    links.classList.toggle("active");
+  });
+
